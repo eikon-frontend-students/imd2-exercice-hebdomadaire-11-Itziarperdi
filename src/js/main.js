33f0596c44e069a404ee05ae5a697a5b7fbd9ba2
Itziarperdi <itziar.peraltadiaz@studentfr.ch>
2026-01-12 16:50:03 +0100

    js annimation couleur et chiffre

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,10 @@
 const likeBtn = document.getElementById("like-Btn");
 const counter = document.querySelector(".like-counter"); // ou utilise un ID si tu en as un
 
+likeBtn.addEventListener("click", function () {
+  likeBtn.classList.toggle("is-active");
+});
+
 let count = 147;
 
 likeBtn.addEventListener("click", () => {
